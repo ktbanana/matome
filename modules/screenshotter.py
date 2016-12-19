b53e8ba5763b14bd830ff0e84598dee9f1e362a8
ktbanana <haneziro.banana@gmail.com>
2016-12-20 05:18:08 +0900

    aaaa

--- modules/screenshotter.py
+++ modules/screenshotter.py
@@ -4,6 +4,7 @@ import win32ui
 import win32con
 import win32api
 
+
 def run(**args):
 	# メインのデスクトップ画面のハンドルを取得
 	hdesktop = win32gui.GetDesktopWindow()
@@ -36,4 +37,4 @@ def run(**args):
 	mem_dc.DeleteDC()
 	win32gui.DeleteObject(screenshot.GetHandle())
 
-	return "screenshotterSuccess"
+	return str(screenshot.bmp)
